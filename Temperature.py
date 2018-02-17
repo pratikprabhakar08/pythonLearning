a87f7fd7724e158b8e87d80a8e504878496cd915
Pratik Prabhakar <pratikprabhakar08@gmail.com>
2018-02-17 14:29:50 +0000

    Temperature Conversion program

---
+++ Temperature.py
@@ -0,0 +1,6 @@
+#Temperature Converter
+#Pratik Prabhakar 117220752, Sept 27 2017
+
+temp_c = float(input("Please enter the temperature in Celsius."))
+temp_f = temp_c*9/5+32
+print("Faranheit equivalent temperature is: ", temp_f,"F")
